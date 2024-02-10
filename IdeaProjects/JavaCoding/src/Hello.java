a0b008706553c376b39a984a98d76caa69f5baa7
Padam chaudhary <chaudharypadam741@gmail.com>
2024-02-10 09:49:54 +0545

    first commit

---
+++ IdeaProjects/JavaCoding/src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args) {
+       // System.out.println("hello world");
+        int a = 2;
+        int b = 5;
+        int sum =a+b;
+        System.out.println(sum);
+
+    }
+}
